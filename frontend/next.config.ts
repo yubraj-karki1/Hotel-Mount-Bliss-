36c5bd5d9db6998c53f5acb2e66b20069d2a700b
Yubraj Karki <yubrajkarki188@gmail.com>
2026-09-13 10:54:57 +0545

    new commit

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: process.env.VERCEL ? undefined : "standalone",
   poweredByHeader: false,
   compress: true,
   async headers() {
